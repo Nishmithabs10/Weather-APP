a816b2240fe190c8033ea694fd0576170a452aad
Nishmithabs10 <nishbs718@gmail.com>
2025-03-17 22:08:34 +0530

    created basic web design

--- Weatherproject/Weatherproject/urls.py
+++ Weatherproject/Weatherproject/urls.py
@@ -1,24 +1,7 @@
-"""
-URL configuration for Weatherproject project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 # from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
     path('', include('weather.urls')),
 ]
-97eba7c5cb0505fde7e9c2d02e5f09b2
-https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+# 97eba7c5cb0505fde7e9c2d02e5f09b2
--- Weatherproject/weather/views.py
+++ Weatherproject/weather/views.py
@@ -1,5 +1,17 @@
-from django.shortcuts import render,HttpResponse
-
+from django.shortcuts import render
+import requests
+import datetime
 # Create your views here.
 def home(request):
-    return HttpResponse("this ids my django server")
+    if 'city' in request.POST:
+         city = request.POST['city']
+    else:
+         city = 'indore'      
+    url=f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid=97eba7c5cb0505fde7e9c2d02e5f09b2'
+    PARAMS = {'units':'metric'}
+    data = requests.get(url,params=PARAMS).json()
+    description = data['weather'][0]['description']
+    icon = data['weather'][0]['icon']
+    temp = data['main']['temp']
+    day = datetime.date.today()
+    return render(request,'weatherapp/index.html' ,{'description':description, 'icon':icon  ,'temp':temp, 'day':day ,'city':city  } )
